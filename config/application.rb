f907009ebb88488d5f61962cd10c55b2ff02d47e
Paul Gruson <p.gruson@gmail.com>
2021-06-02 14:57:21 +0200

    test

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,5 @@ module Funadf
 
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.sass.preferred_syntax = :sass
-    config.sass.line_comments = false
-    config.sass.cache = false
-
   end
 end
