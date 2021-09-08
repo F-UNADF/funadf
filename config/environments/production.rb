e787c78ab3841b0b58aed00caffda441c7262678
Paul Gruson <p.gruson@gmail.com>
2021-09-08 13:37:45 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
