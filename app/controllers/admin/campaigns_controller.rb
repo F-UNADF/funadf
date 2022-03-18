12fe6c5f7814707e3212a945783e24d380e27623
Paul Gruson <p.gruson@gmail.com>
2022-03-18 15:02:11 +0100

    bug fiw campaign admin

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -23,40 +23,37 @@ class Admin::CampaignsController < AdminController
 
   # GET /admin/campaigns/new
   def new
-    @acampaign = Campaign.new
+    @campaign = Campaign.new
+    @structures = Association.order(name: :asc)
   end
 
   # GET /admin/campaigns/1/edit
   def edit
+    @structures = Association.order(name: :asc)
   end
 
   # POST /admin/campaigns
   # POST /admin/campaigns.json
   def create
     @campaign = Campaign.new(campaign_params)
+    @structures = Association.order(name: :asc)
 
-    respond_to do |format|
-      if @campaign.save
-        format.html { redirect_to @campaign, notice: 'Campaign was successfully created.' }
-        format.json { render :show, status: :created, location: @campaign }
-      else
-        format.html { render :new }
-        format.json { render json: @campaign.errors, status: :unprocessable_entity }
-      end
+    if @campaign.save
+      redirect_to [:admin, :campaigns], notice: 'Campaign was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /admin/campaigns/1
   # PATCH/PUT /admin/campaigns/1.json
   def update
-    respond_to do |format|
-      if @campaign.update(campaign_params)
-        format.html { redirect_to @campaign, notice: 'Campaign was successfully updated.' }
-        format.json { render :show, status: :ok, location: @campaign }
-      else
-        format.html { render :edit }
-        format.json { render json: @campaign.errors, status: :unprocessable_entity }
-      end
+    @structures = Association.order(name: :asc)
+
+    if @campaign.update(campaign_params)
+      redirect_to [:admin, :campaigns], notice: 'Campaign was successfully created.', notice: 'Campaign was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -97,6 +94,8 @@ class Admin::CampaignsController < AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def campaign_params
-      params.fetch(:campaign, {})
+      params[:campaign].permit(:structure_id, :name, :description, :code, :start_at, :end_at, :is_public,
+                                motions_attributes: [:id, :name, :kind, :order, :_destroy],
+                                voting_tables_attributes: [:id, :position, :voting, :as_member, :_destroy])
     end
 end
