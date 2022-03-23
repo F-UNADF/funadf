2f2be1d7d999c2a0935f33c01e48d97d79a5ea40
Paul Gruson <p.gruson@gmail.com>
2022-03-23 23:35:20 +0100

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,8 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/sockets/unicorn_1.sock", :backlog => 128
+listen "#{shared_dir}/sockets/unicorn_2.sock", :backlog => 128
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
