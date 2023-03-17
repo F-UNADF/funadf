227e5ed68eed811c8c055ccaee689e85d6711374
Paul Gruson <p.gruson@gmail.com>
2023-03-17 14:01:56 +0100

    hotfix_sam: fix for Catherine & Sam

--- app/controllers/structures/is_memberships_controller.rb
+++ app/controllers/structures/is_memberships_controller.rb
@@ -2,7 +2,13 @@ class Structures::IsMembershipsController < ApplicationController
   before_action :set_structure
 
   def index
-    @is_members = @structure.memberships.paginate(:page => params[:page])
+    @is_members = Membership.select("structures.id AS friendly_id,
+                                    structures.name AS name,
+                                    structures.email AS email,
+                                    structures.phone_1 AS phone_1,
+                                    roles.name AS role")
+                            .joins(:structure, :role)
+                            .where(member_id: @structure.id, member_type: 'Structure')
   end
 
 
