5563e83a423e9937b56fa6904cd2c55b20a7f1c0
Paul Gruson <p.gruson@gmail.com>
2023-09-08 16:27:34 +0200

    feature/VueJSVotes: ending votes

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,9 +2,9 @@ class AvatarsController < ApplicationController
   def show
     user = User.find(params[:user])
     if user.avatar.attached?
-      redirect_to user.avatar.variant(resize_to_fill: [500, 500])
+      send_data user.avatar.variant(resize_to_fill: [500, 500])
     else
-      redirect_to 'https://www.gravatar.com/avatar/' + Digest::MD5.hexdigest(user.email.downcase) + '?s=200&d=mm'
+      send_data 'https://www.gravatar.com/avatar/' + Digest::MD5.hexdigest(user.email.downcase) + '?s=200&d=mm'
     end
   end
 end
--- app/controllers/votes/campaigns_controller.rb
+++ app/controllers/votes/campaigns_controller.rb
@@ -1,72 +1,9 @@
 class Votes::CampaignsController < VotesController
 
   def index
-    @campaigns = Campaign.currents
-
-    @user = current_user
-    @presidences = @user.get_presidences
-
-    campaing_ids = []
-
-    @campaigns.each do |campaign|
-      @presidences.each_with_index do |s, index|
-        if campaign.structure.is_member?(s)
-          if campaign.structure.member_can_vote?(s) && !campaign.has_already_vote?(s)
-            # MEMBRE DE L'ASSOC, CAN_VOTE
-            campaing_ids << campaign.id
-
-            break
-          elsif !campaign.structure.member_can_vote?(s)
-            # MEMBRE DE L'ASSOC, MAIS CANT_VOTE
-            campaing_ids << campaign.id
-
-            break
-          end
-
-
-        elsif campaign.structure_can_vote?(s, false) && !campaign.has_already_vote?(s)
-          # NON MEMBRE, CAN VOTE
-          campaing_ids << campaign.id
-          break
-        end
-      end
-
-      if campaign.structure.is_member?(@user)
-        if campaign.structure.member_can_vote?(@user) && campaign.user_can_vote?(@user) && !campaign.has_already_vote?(@user)
-          # MEMBRE DE L'ASSOC, CAN_VOTE
-          campaing_ids << campaign.id
-
-
-        elsif !campaign.structure.member_can_vote?(@user)
-
-          # MEMBRE DE L'ASSOC, MAIS CANT_VOTE
-          campaing_ids << campaign.id
-        end
-
-      elsif campaign.user_can_vote?(@user, false) && !campaign.has_already_vote?(@user)
-        # NON MEMBRE, CAN VOTE
-        campaing_ids << campaign.id
-      end
-
-    end
-
-    @campaigns = Campaign.where(id: campaing_ids).order(start_at: :asc)
   end
 
   def show
-    @campaign = Campaign.find params[:id]
-    @structure = Structure.find(@campaign.structure_id)
-
-    @user = current_user
-    # GET ALL PRESIDENCES
-    pres = @user.get_presidences
-    @presidences = []
-    pres.each do |s|
-      unless s == @structure
-        @presidences << s
-      end
-    end
-
   end
 
 end
