bd0c9a0ffc43b0d7f4658645982717d69b7b0235
Paul Gruson <p.gruson@gmail.com>
2023-09-20 19:09:42 +0200

    (no branch, rebasing feature/VueJSOnMe): (no branch, rebasing feature/VueJSOnMe): feature/VueJSOnMe: first step of dev  Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.   interactive rebase in progress; onto 6958405  Last commands done (4 commands done):  pick b1a5677 hotfix/assocAdmin: fix admin assoc  pick 9e750be (no branch, rebasing feature/VueJSOnMe): feature/VueJSOnMe: first step of dev  Next commands to do (2 remaining commands):  pick 7c02a99 (no branch, rebasing feature/VueJSOnMe): feature/VueJSOnMe: schem  pick fafd238 feature/VueJSOnMe: keep dev  You are currently rebasing branch 'feature/VueJSOnMe' on '6958405'.   Changes to be committed:  modified: app/controllers/application_controller.rb 

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
 
   before_action :set_subdomain
 
+  layout :set_layout
+
   include PublicActivity::StoreController
 
   def set_subdomain
@@ -35,15 +37,38 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
 
-  def after_sign_in_path_for(resource)
-    uadpif = Structure.where(name: 'UADPIF').first
-    if(resource.structures.include?(uadpif))
-      root_url(subdomain: 'me')
-    else
-      root_url
+  private
+    def set_layout
+      folder = 'votes/'
+      module_name = self.class.to_s.split("::").first
+
+      if module_name.eql?("Devise")
+        return "devise/layouts"
+      else
+        if request.subdomain && request.subdomain != ''
+          case request.subdomain
+          when 'admin'
+            folder = 'admin/'
+          when 'me'
+            folder = 'me/'
+          else
+            folder = 'intranet/'
+          end
+        end
+
+        return "#{folder}layouts/application"
+      end
+
     end
-  end
+
+    def after_sign_in_path_for(resource)
+      uadpif = Structure.where(name: 'UADPIF').first
+      if(resource.structures.include?(uadpif))
+        root_url(subdomain: 'me')
+      else
+        root_url
+      end
+    end
 
 end
