a89f4d654d72824ace8519b9d52f55a569d7456d
Paul Gruson <p.gruson@gmail.com>
2023-10-01 15:57:29 +0200

    (no branch, rebasing feature/VueJSOnMe): feature/VueJSOnMe: Allow user to see image on post

--- app/frontend/components/Feed/Index.vue
+++ app/frontend/components/Feed/Index.vue
@@ -1,25 +1,41 @@
 <template>
-  <v-row class="mt-4">
-    <v-col cols="12" v-for="post in items" :key="post.id">
-      <PostItem :post="post" />
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-row class="mt-4">
+          <v-infinite-scroll :items="items" :onLoad="load">
+            <v-col cols="12" v-for="post in items" :key="post.id">
+              <PostItem :post="post" />
+            </v-col>
+          </v-infinite-scroll>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import {mapGetters} from "vuex";
 import PostItem from "../Posts/me/Item.vue";
+import { VInfiniteScroll } from 'vuetify/labs/VInfiniteScroll'
 
 export default {
   name: "FeedIndex",
   components: {
     PostItem,
+    VInfiniteScroll,
   },
   computed  : {
     ...mapGetters('feedStore', {
       items       : 'getItems',
       loading     : 'getLoading',
     }),
+  },
+  methods   : {
+    async load ({ done }) {
+      const res = await this.api();
+      this.items.push(...res);
+    },
   },
   beforeMount: function () {
     this.$store.dispatch('feedStore/items');
--- app/frontend/components/Posts/me/Item.vue
+++ app/frontend/components/Posts/me/Item.vue
@@ -1,34 +1,104 @@
 <template>
-  <v-card variant="outlined" class="bg-surface">
+  <v-card variant="elevated" class="bg-surface">
     <v-card-item>
       <div class="d-flex gap-3 align-center">
         <v-avatar size="40">
-          <img :src="post.structure_logo_url" width="40" alt="avatar" />
+          <img :src="'/logos/'+post.structure.id+'.png'" width="40" alt="avatar"/>
         </v-avatar>
         <div class="d-block d-sm-flex align-center gap-3">
           <h6 class="text-h6">{{ post.structure.name }}</h6>
+          <v-icon size="x-small" color="grey lighten-5">mdi-circle</v-icon>
           <span class="text-subtitle-2 opacity-50">
-            <CircleIcon size="8" fill="inherit" class="opacity-50 mr-1"/>
-            {{ post.created_at }}
+            {{ dateFormat(post.post.created_at) }}
           </span>
         </div>
       </div>
-      <div class="py-4 text-body-1">
-        {{ post }}
+      <div class="py-4 text-body-1 gap-3">
+        <h3>{{ post.post.title }}</h3>
+        <div v-html="post.post.content"></div>
       </div>
-      <!---If Images
-      <v-row v-if="post?.data.images">
-        <v-col :md="photo.featured ? '12' : '6'" v-for="photo in post?.data.images">
-          <v-avatar class="rounded-md w-100" size="360">
-            <img :src="photo.img" alt="photo" />
-          </v-avatar>
+      <!---If Images -->
+      <v-row v-if="post?.images">
+        <v-col
+            v-for="(photo, index) in post?.images.slice(0, 2)"
+            :key="index"
+            class="d-flex child-flex"
+            cols="4"
+        >
+          <v-img
+              :src="photo"
+              :lazy-src="photo"
+              aspect-ratio="1"
+              cover
+              class="bg-grey-lighten-2"
+          >
+            <template v-slot:placeholder>
+              <v-row
+                  class="fill-height ma-0"
+                  align="center"
+                  justify="center"
+              >
+                <v-progress-circular
+                    indeterminate
+                    color="grey-lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </v-col>
+        <v-col
+            v-if="post?.images.length > 2"
+            class="d-flex child-flex"
+            cols="4"
+        >
+          <v-img
+              :src="post?.images[2]"
+              :lazy-src="post?.images[2]"
+              aspect-ratio="1"
+              cover
+              class="bg-grey-lighten-2"
+              @click="carouselDialog = true"
+          >
+            <template v-slot:placeholder>
+              <v-row
+                  class="fill-height ma-0"
+                  align="center"
+                  justify="center"
+              >
+                <v-progress-circular
+                    indeterminate
+                    color="grey-lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+            <div class="d-flex align-center justify-center fill-height bg-grey-lighten-3 opacity-50 cursor-pointer">
+              <span class="text-h6 font-weight-bold">{{ post?.images.length - 3 }}+</span>
+            </div>
+          </v-img>
         </v-col>
       </v-row>
-      If Video
-      <div v-if="post?.data.video" class="rounded-md overflow-hidden my-5">
-        <iframe :src="`https://www.youtube.com/embed/${post?.data.video}`" frameborder="0" width="100%" height="400"></iframe>
-      </div>
-      Like and comment count
+
+      <!---If Attachments -->
+      <v-row v-if="post?.attachments" class="pb-3">
+        <v-col
+            v-for="(attachment, index) in post?.attachments"
+            :key="index"
+            class="d-flex child-flex"
+            cols="4"
+        >
+          <v-btn
+              :href="attachment"
+              target="_blank"
+              elevation="1"
+              block>
+            <v-icon>mdi-download</v-icon>
+            {{ attachment.split('/').pop() }}
+          </v-btn>
+
+        </v-col>
+      </v-row>
+
+      <!-- Like and comment count
       <div class="my-4 mt-5 d-flex align-center gap-3">
         <v-tooltip text="Like">
           <template v-slot:activator="{ props }">
@@ -94,19 +164,61 @@
         </v-btn>
       </div>
     </div>-->
+    <v-dialog v-model="carouselDialog" max-width="50%">
+      <v-card>
+        <v-card-title>
+          <v-btn icon size="small" color="red" @click="carouselDialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-carousel show-arrows="hover" hide-delimiters>
+            <v-carousel-item v-for="(photo, index) in post?.images" :key="index">
+              <v-img
+                  :src="photo"
+                  :lazy-src="photo"
+                  class="bg-grey-lighten-2"
+              >
+                <template v-slot:placeholder>
+                  <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                  >
+                    <v-progress-circular
+                        indeterminate
+                        color="grey-lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+            </v-carousel-item>
+          </v-carousel>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
 import {mapActions} from "vuex";
+import moment from "moment/moment";
 
 export default {
-  name: "PostItem",
-  props: {
+  name   : "PostItem",
+  props  : {
     post: {
-      type: Object,
+      type    : Object,
       required: true,
     },
   },
+  methods: {
+    dateFormat: function (value) {
+      return moment(value).format('DD/MM/YYYY HH:mm');
+    },
+  },
+  data   : () => ({
+    carouselDialog: false,
+  }),
 }
 </script>
