6ca0f9432be19e1326c9c2122024ae8295152a9a
Paul Gruson <p.gruson@gmail.com>
2023-10-02 22:49:32 +0200

    hotfix_Vote: fix display voteé

--- app/frontend/components/Votes/Show.vue
+++ app/frontend/components/Votes/Show.vue
@@ -1,99 +1,101 @@
 <template>
-  <v-row>
-    <v-col cols="12" lg="12">
-      <!-- Button to go back to the list -->
-      <v-btn color="primary" @click="$router.push({name: 'votes.index'})" variant="text" size="small" class="mb-5">
-        <template v-slot:prepend>
-          <v-icon>mdi-arrow-left</v-icon>
-        </template>
-        <template v-slot:default>
-          Retour à la liste
-        </template>
-      </v-btn>
-      <h4 class="text-h5 font-weight-bold mb-4">
-        {{ structure.name }}
-        <small>{{ item.name }}</small>
-      </h4>
-    </v-col>
-    <v-col cols="12" lg="8">
-      <v-card class="mb-5" v-for="motion in editResult" :key="motion.motion_id">
-        <v-card-text class="d-flex justify-space-around flex-wrap">
-          <p class="text-center mb-4">
-            <b>
-              {{ motion.name }}
-            </b>
-          </p>
-          <v-btn-toggle divided elevation="2" border v-model="motion.vote" mandatory v-if="motion.kind === 'binary'">
-            <v-btn color="primary" value="oui">
-              OUI
-            </v-btn>
-            <v-btn color="primary" value="non">
-              NON
-            </v-btn>
-          </v-btn-toggle>
+  <v-container max-width="650px">
+    <v-row>
+      <v-col cols="12" lg="12">
+        <!-- Button to go back to the list -->
+        <v-btn color="primary" @click="$router.push({name: 'votes.index'})" variant="text" size="small" class="mb-5">
+          <template v-slot:prepend>
+            <v-icon>mdi-arrow-left</v-icon>
+          </template>
+          <template v-slot:default>
+            Retour à la liste
+          </template>
+        </v-btn>
+        <h4 class="text-h5 font-weight-bold mb-4">
+          {{ structure.name }}
+          <small>{{ item.name }}</small>
+        </h4>
+      </v-col>
+      <v-col cols="12" lg="8">
+        <v-card class="mb-5" v-for="motion in editResult" :key="motion.motion_id">
+          <v-card-title>
+            <small><b>{{ motion.name }}</b></small>
+          </v-card-title>
+          <v-card-text class="justify-content-center pa-3">
 
-          <v-btn-toggle justify-center divided elevation="2" border v-model="motion.vote" mandatory
-                        v-else-if="motion.kind === 'neutral'">
-            <v-btn color="primary" value="oui">
-              OUI
-            </v-btn>
-            <v-btn color="primary" value="non">
-              NON
-            </v-btn>
-            <v-btn color="primary" value="neutre">
-              NEUTRE
-            </v-btn>
-          </v-btn-toggle>
+            <v-btn-toggle flat divided elevation="2" border v-model="motion.vote" mandatory
+                          v-if="motion.kind === 'binary'">
+              <v-btn color="primary" value="oui">
+                OUI
+              </v-btn>
+              <v-btn color="primary" value="non">
+                NON
+              </v-btn>
+            </v-btn-toggle>
 
-          <v-btn-toggle justify-center divided elevation="2" border v-model="motion.vote" mandatory
-                        v-else-if="motion.kind === 'choices'">
-            <v-btn v-for="choice in motion.choices.split(',')" color="primary" :value="choice">
-              {{ choice }}
-            </v-btn>
-          </v-btn-toggle>
+            <v-btn-toggle flat justify-center divided elevation="2" border v-model="motion.vote" mandatory
+                          v-else-if="motion.kind === 'neutral'">
+              <v-btn color="primary" value="oui">
+                OUI
+              </v-btn>
+              <v-btn color="primary" value="non">
+                NON
+              </v-btn>
+              <v-btn color="primary" value="neutre">
+                NEUTRE
+              </v-btn>
+            </v-btn-toggle>
 
-          <v-text-field v-model="motion.vote" placeholder="Réponse libre" v-else-if="motion.kind === 'free'">
-          </v-text-field>
-        </v-card-text>
-      </v-card>
-    </v-col>
-    <v-col cols="12" lg="4">
-      <v-card class="mb-5">
-        <v-list select-strategy="classic">
-          <v-list-subheader>Mes bulletins</v-list-subheader>
+            <v-btn-toggle flat justify-center divided elevation="2" border v-model="motion.vote" mandatory
+                          v-else-if="motion.kind === 'choices'">
+              <v-btn v-for="choice in motion.choices.split(',')" color="primary" :value="choice">
+                {{ choice }}
+              </v-btn>
+            </v-btn-toggle>
 
-          <v-list-item v-for="voter in this.editVoters">
-            <v-list-item-title v-if="voter.has_voted === null">
-              <v-checkbox v-model="voter.selected">
-                <template v-slot:label>
-                  {{ voter.name }}&nbsp;
-                  <small v-if="voter.is_consultative === 1">(Vote consultatif)</small>
-                  <small v-if="voter.is_consultative === 0">(Vote comptabilisé)</small>
-                </template>
-              </v-checkbox>
-            </v-list-item-title>
-            <v-list-item-title v-else>
-              <em>{{ voter.name }} a déjà voté</em>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-card>
+            <v-text-field v-model="motion.vote" placeholder="Réponse libre" v-else-if="motion.kind === 'free'">
+            </v-text-field>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="4">
+        <v-card class="mb-5">
+          <v-list select-strategy="classic">
+            <v-list-subheader>Mes bulletins</v-list-subheader>
 
-    </v-col>
-    <v-col cols="12" lg="12">
-      <v-btn block color="primary" @click="goVote()">
-        <template v-slot:prepend>
-          <v-icon>mdi-alert</v-icon>
-        </template>
-        <template v-slot:default>
-          Soumettre mon vote
-        </template>
-        <template v-slot:append>
-          <v-icon>mdi-alert</v-icon>
-        </template>
-      </v-btn>
-    </v-col>
-  </v-row>
+            <v-list-item v-for="voter in this.editVoters">
+              <v-list-item-title v-if="voter.has_voted === null">
+                <v-checkbox v-model="voter.selected">
+                  <template v-slot:label>
+                    {{ voter.name }}&nbsp;
+                    <small v-if="voter.is_consultative === 1">(Vote consultatif)</small>
+                    <small v-if="voter.is_consultative === 0">(Vote comptabilisé)</small>
+                  </template>
+                </v-checkbox>
+              </v-list-item-title>
+              <v-list-item-title v-else>
+                <em>{{ voter.name }} a déjà voté</em>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+
+      </v-col>
+      <v-col cols="12" lg="12">
+        <v-btn block color="primary" @click="goVote()">
+          <template v-slot:prepend>
+            <v-icon>mdi-alert</v-icon>
+          </template>
+          <template v-slot:default>
+            Soumettre mon vote
+          </template>
+          <template v-slot:append>
+            <v-icon>mdi-alert</v-icon>
+          </template>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -172,6 +174,18 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style>
+.v-btn-toggle{
+  height: auto !important;
+  display: flex;
+  flex-direction: column;
+}
+html .v-btn-toggle > .v-btn{
+  height: 48px !important;
+  border: none !important;
+  border-bottom: solid 1px #ccc !important;
+}
+html .v-btn-toggle > .v-btn:last-child{
+  border-bottom: none !important;
+}
 </style>
