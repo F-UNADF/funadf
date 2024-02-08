ff21822d4455a02c7343a6192bf8e95958ded9da
Paul GRUSON <p.gruson@gmail.com>
2024-02-08 11:21:00 +0100

    Fix Acces Admin

--- app/controllers/v1/votes_controller.rb
+++ app/controllers/v1/votes_controller.rb
@@ -14,53 +14,6 @@ module V1
 
       campaing_ids = @user.eligible_campaign_ids
 
-      # @campaigns = Campaign.currents
-      # @presidences = @user.get_presidences
-
-      # campaing_ids = []
-
-      # @campaigns.each do |campaign|
-      #   @presidences.each_with_index do |s, index|
-      #     if campaign.structure.is_member?(s)
-      #       if campaign.structure.member_can_vote?(s) && !campaign.has_already_vote?(s)
-      #         # MEMBRE DE L'ASSOC, CAN_VOTE
-      #         campaing_ids << campaign.id
-
-      #         break
-      #       elsif !campaign.structure.member_can_vote?(s)
-      #         # MEMBRE DE L'ASSOC, MAIS CANT_VOTE
-      #         campaing_ids << campaign.id
-
-      #         break
-      #       end
-
-
-      #     elsif campaign.structure_can_vote?(s, false) && !campaign.has_already_vote?(s)
-      #       # NON MEMBRE, CAN VOTE
-      #       campaing_ids << campaign.id
-      #       break
-      #     end
-      #   end
-
-      #   if campaign.structure.is_member?(@user)
-      #     if campaign.structure.member_can_vote?(@user) && campaign.user_can_vote?(@user) && !campaign.has_already_vote?(@user)
-      #       # MEMBRE DE L'ASSOC, CAN_VOTE
-      #       campaing_ids << campaign.id
-
-
-      #     elsif !campaign.structure.member_can_vote?(@user)
-
-      #       # MEMBRE DE L'ASSOC, MAIS CANT_VOTE
-      #       campaing_ids << campaign.id
-      #     end
-
-      #   elsif campaign.user_can_vote?(@user, false) && !campaign.has_already_vote?(@user)
-      #     # NON MEMBRE, CAN VOTE
-      #     campaing_ids << campaign.id
-      #   end
-
-      # end
-
       render json: { campaigns: Campaign.where(id: campaing_ids) }, include: [:structure => { only: :name }]
     end
 
