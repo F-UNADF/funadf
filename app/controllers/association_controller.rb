8d6dbd8f12af2c68f10e4ccd9e2c638cf8ff51ba
Paul GRUSON <paul.gruson@conecs.fr>
2024-09-13 22:19:09 +0200

    Clean controllers + Switch User

--- app/controllers/association_controller.rb
+++ app/controllers/association_controller.rb
@@ -1,14 +1,3 @@
 class AssociationController < ApplicationController
   layout 'association/layouts/vuejs'
-
-  before_action :user_has_responsabilities?
-
-  private
-
-  def user_has_responsabilities?
-    if current_user.associations_responsabilities.any?
-      return true
-    end
-    redirect_to root_url(subdomain: 'me'), alert: 'Vous n\'êtes pas autorisé à consulter cette page'
-  end
 end
