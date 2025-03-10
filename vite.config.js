64c84f0ebd6aeee0777e4ab2c65e6ca5409bd3ee
Paul GRUSON <p.gruson@gmail.com>
2025-03-10 20:55:21 +0100

    master: build vite conf

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,11 @@ export default defineConfig({
         RubyPlugin(),
         vue(),
     ],
-    build: {
-        manifest: true,
-        outDir: 'public/vite',
-        emptyOutDir: true, // Efface le dossier avant chaque build
-        rollupOptions: {
-            input: 'app/frontend/entrypoints.js'
-        },
-    },
     server: {
         host: '0.0.0.0',
         port: 3036,
         allowedHosts: true,
         strictPort: true,
     },
-    cacheDir: 'public/vite/.cache', // Déplace le cache de Vite pour éviter .vite/
     base: '/app/frontend/',
 });
