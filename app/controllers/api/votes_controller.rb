5b48ce56d76a7741e62e52a8b91cf21bf801361f
Paul GRUSON <paul.gruson@conecs.fr>
2025-07-15 13:37:02 +0200

    Presence true

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -105,7 +105,7 @@ class Api::VotesController < ApiController
       structure: @campaign.structure, 
       motions: @campaign.motions.as_json, 
       voters: results.as_json,
-      presence: true,
+      present: true,
     }
   end
 
